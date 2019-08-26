85ee8f5fa02797f7551f3382beb3bad57ccbfc06
eduardagoulart <goularteduarda.a@gmail.com>
2019-08-26 15:05:41 -0300

    UPDATE: apenas mudando o print

--- ganho.py
+++ ganho.py
@@ -1,17 +1,14 @@
 from math import log
 
 raiz = float(input("Entropia do nó acima desse: "))
-qtd = int(input("Quantidade de elementos: "))
+qtd = int(input("Quantidade total de elementos: "))
 qtd_elem = int(input("Quantidade de estados: "))
 
 ganho = 0
 for i in range(qtd_elem):
     x = int(input(f"quantidade do estado {i}: "))
-    entropia = float(input("Entropia desse estado: "))
+    entropia = float(input(f"Entropia desse estado {i}: "))
     ganho += ((x/qtd) * entropia)
-    print(ganho)
 
 
-print(raiz)
-print(ganho)
 print(raiz - ganho)
